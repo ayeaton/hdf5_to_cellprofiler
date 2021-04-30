1a8465ea29f8c478d28d86f68bd88f1cd34cbfcc
ayeaton <ay1392@bigpurple-ln2.cm.cluster>
2021-04-30 08:16:59 -0400

    continue tidying

--- hdf5_to_cellprofiler.py
+++ hdf5_to_cellprofiler.py
@@ -42,23 +42,19 @@ def parse_args():
                             default = "rank",
                             help="name to append files")
     args = parser.parse_args()
-
     print("Inputs:")
     for arg in vars(args):
         print("{} is {}".format(arg, getattr(args, arg)))
-
     return args
 
 
 def chunk_indices(input, chunks, rank):
-
     with h5py.File(input, 'r') as file:
         # get the keys in the dataset
         keys =  list(file.keys())
         # get the number of entries
         f_tiles = file[keys[0]]
         num_images = f_tiles.shape[0]
-
     # size of the chunks
     chunk_size, remainder = divmod(num_images, chunks)
     chunk_size = chunk_size + 1
@@ -72,12 +68,12 @@ def chunk_indices(input, chunks, rank):
         start = int(rank * chunk_size)
         end = int(start + chunk_size)
         print("rank: " + str(rank) + " start: " + str(start) + " end: " + str(end))
-
     return start, end
 
 
 def main():
 
+    # get arguments and rename
     args = parse_args()
     input = args.input
     working_dir = args.working_dir
@@ -87,15 +83,17 @@ def main():
     rank = args.rank - 1
     chunks = args.chunks
 
-    ## get the indices of the hdf5 file
+    ## get the indices of the hdf5 file this script will work on
     start, end = chunk_indices(input, chunks, rank)
 
     # read files and write
     with h5py.File(input, 'r') as f:
-        #get unique labels for each entry
+        keys =  list(f.keys())
+        # subset the label keys
         datasets_subbed =  [f[key][start:end, ] for key in keys if len(f[key][start:end, ].shape) == 1]
+        # standardize the labels
         unique_information_str =  [[str(x.decode("utf-8")) if hasattr(x, 'decode') else str(x) for x in key]  for key in datasets_subbed]
-        # assume image dataset is the only one that is not one dimensional
+        # assume image dataset is the only one that is not one dimensional and get image data
         f_img = [f[key][start:end, ] for key in keys if len(f[key][start:end, ].shape) != 1 ]
         f_img = f_img[0]
         ## write the images to a temp file
@@ -112,7 +110,7 @@ def main():
     ## create a file listing all the images
     file_path = temp_path + '/file_list.txt'
     l = list(glob.glob(temp_path + '/*.jpeg'))
-    with open(file_path, 'r') as f1:
+    with open(file_path, 'w') as f1:
         s1 = '\n'.join(l)
         f1.write(s1)
 
@@ -120,20 +118,24 @@ def main():
     proc = subprocess.call([os.path.join(os.path.abspath(sys.path[0]), "cellprofiler.sh"),
                              file_path,
                              working_dir])
-
+    # make output directory if it doesnt exist
     if not os.path.exists(output):
         os.makedirs(output)
 
-    os.rename(temp_path + '/nuclei.txt', output + '/nuclei'  + str(name) + str(rank)  +  '.txt')
-    os.rename(temp_path + '/file_list.txt', output + '/file_list' + str(name) + str(rank)  + '.txt')
-    os.rename(temp_path + '/Image.txt', output + '/Image' + str(name) + str(rank) + '.txt')
+    # move the cellprofiler output to the output
+    os.rename("% s/nuclei.txt" % temp_path, "% s/nuclei% s% s.txt" % (output, str(name), str(rank)))
+    os.rename("% s/file_list.txt" % temp_path, "% s/file_list% s% s.txt" % (output, str(name), str(rank)))
+    os.rename("% s/Image.txt" % temp_path, "% s/Image% s% s.txt" % (output, str(name), str(rank)))
 
     # remove temp
+    # remove images
     for fn in l:
        os.remove(fn)
+    # remove text files
     l2 = list(glob.glob(temp_path + '/*.txt'))
     for fn in l2:
        os.remove(fn)
+    # remove dir
     os.rmdir(temp_path)
 
 
